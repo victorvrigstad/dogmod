26be1af82d485c22558de7a9ef096387907f4e0d
victor <victor.vrigstad@gmail.com>
2024-05-22 21:25:37 +0200

    first dog push

---
+++ src/main/java/net/victor/dogmod/DogModClient.java
@@ -0,0 +1,10 @@
+package net.victor.dogmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DogModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/victor/dogmod/DogModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.victor.dogmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DogModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
